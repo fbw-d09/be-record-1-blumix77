e9c166d7c35fdc64935093e3d4f0d2f6190f8583
benni <bstolp@gmx.de>
2023-08-10 16:08:58 +0200

    delete dont works but nobody knows why

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 // POST 
 
-const create = ({ id, title, artist, year, cover, price }) => 
+/* const create = ({ id, title, artist, year, cover, price }) => 
 {
     const newRecord = {
         id,
@@ -32,21 +32,19 @@ const create = ({ id, title, artist, year, cover, price }) =>
         price
     }
 
-    db.get("records").push(newRecord).write();
-
     console.log("Eintrag erstellt:", newRecord);
-}
-
-/* create({
-    id: 1,
+} */
+/* 
+create({
+    id: 5,
     title: "testTitle",
     artist: "testArtist",
     year: "1978",
     cover: "DefaultCover",
     price: "3.5664€"
-})
+}) */
 
-create({
+/* create({
     id: 2,
     title: "testTitle2",
     artist: "testArtist2",
@@ -54,8 +52,8 @@ create({
     cover: "DefaultCover",
     price: "18€"
 
-}) */
-
+})  */
+/* 
 create({
     id: 3,
     title: "testTitle3",
@@ -64,43 +62,53 @@ create({
     cover: "DefaultCover",
     price: "20€"
 
-})
+}) */
 
 // GET 
 
-const readOne = (id) => 
+/* const readOne = (id) => 
 {
     const selectedRecord = db.get("records").find({ id }).value();
 
     console.log("Einzelner ausgegebener Eintrag:", selectedRecord);
 
-}
+} */
 
 // UPDATE / PUT 
 
-const update = (id, title, artist, year, cover, price) => 
+/* const update = (id, title, artist, year, cover, price) => 
 {
     db.get("records").find({ id })
 
     .assign({ title, artist, year, cover, price }).write();
 
     console.log("Update erfolgreich!");
-}
+} */
 
 // update(1, "dasdasd"); 
 
 // DELETE 
 
-const remove = (id) => 
+/* const remove = (id) => 
 {
 
     db.get("records").remove({ id }).write();
 
     console.log(`Record mit der ID: ${id} wurde gelöscht!`)
 
-}
+} */
 
 // remove(1)
+
+app.delete("/api/records/delete/:id", async (req, res) => {
+
+    console.log("METHOD: ", req.method);
+    const { id } = req.params;
+    console.log(id);
+    await db.get("records").remove({id: 2}).write();
+    //console.log(req.params.id);
+    res.status(200).json({ success: true })
+})
 
 app.get("/api/records", (req, res) => 
 {   
@@ -109,9 +117,9 @@ app.get("/api/records", (req, res) =>
 
     const { newRecord } = req.body;
 
+    const selectedRecord = db.get("records").value();
 
-    res.status(200).json({ success: true, data: []})
-
+    res.status(200).json({success: true, selectedRecord });
 
 });
 
@@ -120,13 +128,14 @@ app.post("/api/records", (req, res) =>
 
     console.log("METHOD:", req.method);
 
-    res.status(200).json({ success: true, data: []})
+    
     // res.status(200).json(`New Record ${newRecord} added!`)
-    const { record } = req.body;
+    const { id, title, artist, year, cover, price } = req.body;
+    const newArtist = { id, title, artist, year, cover, price }
 
-    res,send(record);
+    db.get("records").push( newArtist ).write();
 
-
+    res.status(200).json({ success: true, data: newArtist})
 });
 
 app.listen(port, () => console.log("Server läuft auf Port: ", port));
